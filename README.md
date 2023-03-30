d2a93c7235ffdb4ace5735ec5199b52333959bcd
s561417 <123023199+s561417@users.noreply.github.com>
2023-03-29 23:41:17 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # s23wa21nuvvula
+
+Render link : https://s23wa21nuvvula-1ejj.onrender.com
--- routes/compute.js
+++ routes/compute.js
@@ -5,19 +5,23 @@ var x,y;
 router.get('/', function(req, res, next) {
     if(req.query.x == undefined){
         x=Math.random()*10;
-        y=Math.random()*10;
+        //y=Math.random()*10;
         
         var tanval = Math.tan(x) 
-        var sqrtval = Math.sqrt(y);
+        var sqrtval = Math.sqrt(x);
+        var cos = Math.acosh(x);
+        var logl = Math.log1p(x);
         
-        res.send('\nMath.tan() applied for ' +x +' is ' +tanval+'\n Math.sqrt() applied for number ' +y+' is '+sqrtval);
+        res.send('\nMath.tan() applied for ' +x +' is ' +tanval+'\n Math.sqrt() applied for number ' +x+' is '+sqrtval+ ' Math.tan() applied for ' +x +' is ' +cos+' Math.tan() applied for ' +x +' is ' +logl);
     }
     else{
         x =req.query.x;
       
         var tanval = Math.tan(x) 
         var sqrtval = Math.sqrt(y) ;
-        res.send('\nMath.tan() applied for ' + x +' is ' +tanval+'\n Math.sqrt() applied for number ' +y+' is '+sqrtval);
+        var cos = Math.acosh(x);
+        var logl = Math.log1p();
+        res.send('\nMath.tan() applied for ' +x +' is ' +tanval+'\n Math.sqrt() applied for number ' +x+' is '+sqrtval+ '\nMath.tan() applied for ' +x +' is ' +cos+ '\nMath.tan() applied for ' +x +' is ' +logl);
     }
 });
 module.exports = router; var tanval = Math.tan(x)
